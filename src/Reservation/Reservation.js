0c90c44c0c393cfb8cdb8af146ba2ad14e65aa29
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-17 09:51:33 -0600

    Render resy with cards and reservation components

---
+++ src/Reservation/Reservation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Reservation.css'
+
+
+
+const Reservation = ({id, name, date, time, number}) => {
+  return (
+    <section data-id={id} className='section section-reservation'>
+      <div>
+        <h2>{name}</h2>
+        <p>{date}</p>
+        <p>{time}</p>
+        <p>Number of Guests: {number}</p>
+      </div> 
+    </section>
+  )
+}
+
+
+
+export default Reservation;
