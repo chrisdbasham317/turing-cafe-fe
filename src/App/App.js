5c082c83d5a27a3aff4cbfa570bfdea42a76a2f8
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-17 09:25:22 -0600

    Add get fetch to cmoponent did mount for reservations

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      reservations: []
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          isLoading: false,
+          reservations: data
+        })
+      })
+  }
+
   render() {
+    const resData = !this.state.isLoading ? 
+       console.log(this.state.reservations) : console.log('not yet')
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
